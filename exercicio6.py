b9a0221eccc8f51f0d7109286cede9d6b7de8437
adriansito124 <adriangobara@gmail.com>
2024-09-13 14:48:22 -0300

    exercicio6

---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+letra = input("Informe a letra: ")
+letra = letra.upper()
+
+if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+    print("é vogal. \n")
+else:
+    print("é consoante. \n")
+
