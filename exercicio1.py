f137242fe8f47c18e89e0c954b44ffc0ac3df7a0
adriansito124 <adriangobara@gmail.com>
2024-09-13 14:27:26 -0300

    exercicio2

--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,4 @@
--- coding: UTF-8 --
-
-sal = float(input(u"QUAL O SAL�RIO:"))
+sal = float(input("QUAL O SAL�RIO:"))
 
 if sal > 5000:
     print("MAIOR QUE R$5.000,00")
---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+mat1 = int(input("QUAL A MATRICULA: "))
+age = int(input("QUAL A IDADE: "))
+mat2 = int(input("QUAL A MATRICULA DA OUTRA: "))
+age2 = int(input("QUAL A IDADE DA OUTRA: "))
+
+if age > age2:
+    print("A MAIOR IDADE INFORMADA FOI", age,"ANOS.\n")
+else:
+    print("A MAIOR IDADE INFORMADA FOI", age2,"ANOS.\n")
