d8cb162321bce10f384564016d3bebd1d2484c02
adriansito124 <adriangobara@gmail.com>
2024-09-13 14:43:47 -0300

    exercicio5

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+age = int(input("Informe a Idade:"))
+
+if age >= 18 and age <= 65:
+    print("ESTA COMPREENDIDA ENTRE 18 E 65 ANOS. \n")
+else:
+    print("NÃO ESTA COMPREENDIDA ENTRE 18 E 65 ANOS. \n")
+
